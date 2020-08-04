a0c8c0c65dfbec67c1eb6b4c6801fc0a63233e34
Felipe Araujo <devaraujofelipe@gmail.com>
2020-08-03 23:22:15 -0300

    Style dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,64 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+import logoImage from '../../assets/logo.svg';
+
+import { Form, Repositories, Title } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <img src={logoImage} alt="GitHub Explorer" />
+      <Title>Explore repositórios no GitHub.</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="test">
+          <img
+            src="https://avatars0.githubusercontent.com/u/12715851?s=460&u=dd3a9f01bafe91c5ac12171f2431f40c382ec4ed&v=4"
+            alt="Felipe"
+          />
+
+          <div>
+            <strong>felipefa/github-explorer</strong>
+            <p>Testing</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="test">
+          <img
+            src="https://avatars0.githubusercontent.com/u/12715851?s=460&u=dd3a9f01bafe91c5ac12171f2431f40c382ec4ed&v=4"
+            alt="Felipe"
+          />
+
+          <div>
+            <strong>felipefa/github-explorer</strong>
+            <p>Testing</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="test">
+          <img
+            src="https://avatars0.githubusercontent.com/u/12715851?s=460&u=dd3a9f01bafe91c5ac12171f2431f40c382ec4ed&v=4"
+            alt="Felipe"
+          />
+
+          <div>
+            <strong>felipefa/github-explorer</strong>
+            <p>Testing</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
+};
 
 export default Dashboard;
---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,94 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+export const Title = styled.h1`
+  color: #3a3a3a;
+  font-size: 48px;
+  line-height: 56px;
+  margin-top: 80px;
+  max-width: 450px;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  margin-top: 40px;
+  max-width: 700px;
+
+  input {
+    border: 0;
+    border-radius: 5px 0 0 5px;
+    color: #3a3a3a;
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+
+  button {
+    background: #04d361;
+    border: 0;
+    border-radius: 0 5px 5px 0;
+    color: #fff;
+    font-weight: bold;
+    height: 70px;
+    transition: background-color 0.2s;
+    width: 210px;
+
+    &:hover {
+      background: ${shade(0.2, '#04d361')};
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    align-items: center;
+    background: #fff;
+    border-radius: 5px;
+    display: flex;
+    padding: 24px;
+    text-decoration: none;
+    transition: transform 0.2s;
+    width: 100%;
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    img {
+      border-radius: 50%;
+      height: 64px;
+      width: 64px;
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong {
+        color: #3d3d4d;
+        font-size: 20px;
+      }
+
+      p {
+        color: #a8a8b3;
+        font-size: 18px;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      color: #c9c9d4;
+      margin-left: auto;
+    }
+  }
+`;
